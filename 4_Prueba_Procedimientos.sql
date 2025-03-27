9a46aebb0087765071690e8abf58d3042b92ca49
marianoaugustok <MARIANOGONZALEZ1993@GMAIL.COM>
2025-03-27 20:14:38 -0300

    Delete 4_Prueba_Procedimientos.sql

--- 4_Prueba_Procedimientos.sql
+++
@@ -1,4 +0,0 @@
-USE gestionViajes;				/*Uso mi DB*/
-/*------------------------------------------------------------------Prueba de procedures --------------------------------------------------------------------*/
-CALL Pr_temporada_dest('Cordoba');
-CALL Pr_paquete_por_nombre('Juan Alvarez');
