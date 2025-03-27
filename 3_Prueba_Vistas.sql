bb1060d2793886b5220c4fe72b505e51812b7baa
marianoaugustok <MARIANOGONZALEZ1993@GMAIL.COM>
2025-03-27 20:13:57 -0300

    Delete 3_Prueba_Vistas.sql

--- 3_Prueba_Vistas.sql
+++
@@ -1,5 +0,0 @@
-USE gestionViajes;				/*Uso mi DB*/
-/*-------------------------------------------------------------------Prueba de vistas --------------------------------------------------------------------*/
-SELECT * FROM Vw_Rrhh; 
-SELECT * FROM Vw_Ventas_paq; 
-SELECT * FROM Vw_Gerencia; 
